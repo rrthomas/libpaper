fe60dd45b13e59fd35e9698e794ee93da34726f8
Reuben Thomas <rrt@sc3d.org>
2013-11-08 22:11:35 +0000

    Remove out-of-date documentation from paper.h

--- libpaper/paper.h
+++ libpaper/paper.h
@@ -9,18 +9,6 @@
 #ifndef PAPER_H
 #define PAPER_H
 
-/*
- * systempapername() returns the preferred paper size got from either the
- *   PAPER environment variable or the /etc/papersize file. If 0 is
- *   returned, one should use the value of defaultpapername().
- *
- * paperinfo() looks from the given paper name in a table of known sizes
- *   and returns 0 if it found it; if this is the case, the width and
- *   height arguments have been set to the correct values in points (one
- *   inch contains 72 points). The case of paper size is not significant.
- *
- */
-
 #ifdef __cplusplus
 extern "C" {
 #endif
