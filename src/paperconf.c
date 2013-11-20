1cd7bf43d2fd100ebc3196c157002b64f35cc56f
Reuben Thomas <rrt@sc3d.org>
2013-11-20 23:22:32 +0000

    Use unitfactor from libpaper to allow paperconf to display dimensions in any known unit

--- src/paperconf.c
+++ src/paperconf.c
@@ -14,7 +14,7 @@
 static void usage(const char* name)
 {
     fprintf(stderr,
-	"Usage: %s [[-p] PAPERNAME|-a] [-z] [-n|-N] [-s|-w|-h] [-c|-m|-i]\n",
+	"Usage: %s [[-p] PAPERNAME|-a] [-z] [-n|-N] [-s|-w|-h] [-u UNIT]\n",
 	    name);
     exit(EXIT_FAILURE);
 }
@@ -23,16 +23,10 @@ static void usage(const char* name)
 #define OPT_UPPERNAME	2
 #define OPT_WIDTH	4
 #define OPT_HEIGHT      8
-#define OPT_CM         16
-#define OPT_MM         32
-#define OPT_INCH       64
-#define OPT_CONTINUE  128
+#define OPT_UNIT       16
+#define OPT_CONTINUE   32
 
-#define OPT_UNIT      (OPT_CM | OPT_MM | OPT_INCH)
-
-#define INCHxCM 2.54
-
-static void printinfo(const struct paper* paper, int options)
+static void printinfo(const struct paper* paper, int options, double dim, const char *unit)
 {
     int pr = 0;
 
@@ -54,24 +48,16 @@ static void printinfo(const struct paper* paper, int options)
 
     if (options & OPT_WIDTH) {
 	if (pr) putchar(' ');
-        if (options & OPT_CM)
-          printf("%g cm", paperpswidth(paper) / 72.0 * INCHxCM );
-	else if (options & OPT_MM)
-          printf("%g mm", paperpswidth(paper) / 72.0 * 10 * INCHxCM );
-	else if (options & OPT_INCH)
-          printf("%g\"", paperpswidth(paper) / 72.0 );
+        if (options & OPT_UNIT)
+          printf("%g %s", paperpswidth(paper) / dim, unit );
 	else
           printf("%g", paperpswidth(paper) );
 	pr = 1;
     }
     if (options & OPT_HEIGHT) {
 	if (pr) putchar(' ');
-        if (options & OPT_CM)
-          printf("%g cm", paperpsheight(paper) / 72.0 * INCHxCM );
-	else if (options & OPT_MM)
-          printf("%g mm", paperpsheight(paper) / 72.0 * 10 * INCHxCM );
-	else if (options & OPT_INCH)
-          printf("%g\"", paperpsheight(paper) / 72.0 );
+        if (options & OPT_UNIT)
+          printf("%g %s", paperpsheight(paper) / dim, unit );
 	else
           printf("%g", paperpsheight(paper) );
 	pr = 1;
@@ -85,10 +71,11 @@ int main(int argc, char** argv)
     set_program_name(argv[0]);
     setlocale(LC_ALL, "");
 
-    const char* paper = NULL;
+    const char *paper = NULL, *unit = NULL;
+    double dim = 1.0;
     int c, all = 0;
     unsigned options = 0;
-    while ((c = getopt(argc, argv, "aznNswhcmip:")) != EOF) {
+    while ((c = getopt(argc, argv, "aznNswhp:u:")) != EOF) {
 	switch (c) {
 	    case 'a':
 		if (paper)
@@ -128,19 +115,11 @@ int main(int argc, char** argv)
 		options |= OPT_HEIGHT;
 		break;
 
-	    case 'c':
-	        if (options & OPT_UNIT) usage(program_name);
-		options |= OPT_CM;
-		break;
-
-	    case 'm':
-	        if (options & OPT_UNIT) usage(program_name);
-		options |= OPT_MM;
-		break;
-
-	    case 'i':
-	        if (options & OPT_UNIT) usage(program_name);
-		options |= OPT_INCH;
+	    case 'u':
+                options |= OPT_UNIT;
+                unit = optarg;
+                dim = unitfactor(unit);
+                if (dim == 0) usage(program_name);
 		break;
 
 	    default:
@@ -158,13 +137,13 @@ int main(int argc, char** argv)
 
     if (all) {
 	for (const struct paper* papers = paperfirst(); papers; papers = papernext(papers))
-	    printinfo(papers, options);
+	    printinfo(papers, options, dim, unit);
     } else {
         if (!paper) paper = systempapername();
 
         const struct paper* syspaper = paperinfo(paper);
         if (syspaper) {
-            printinfo(syspaper, options);
+            printinfo(syspaper, options, dim, unit);
         } else {
 	    fprintf(stderr, "%s: unknown paper `%s'\n", program_name, paper);
 	    if (options & OPT_CONTINUE) {
