af0e32d87c64a365dcbd2dd30798381989bc72e3
Reuben Thomas <rrt@sc3d.org>
2021-11-22 12:24:54 +0000

    Add AppVeyor CI

---
+++ build-aux/appveyor-build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Build on AppVeyor
+# Written by Reuben Thomas 2021.
+# This file is in the public domain.
+
+set -e
+
+./bootstrap
+CONFIGURE_ARGS=(--enable-relocatable)
+if [[ "$ASAN" == "yes" ]]; then
+    CONFIGURE_ARGS+=(CFLAGS="-g3 -fsanitize=address -fsanitize=undefined" CXXFLAGS="-g3 -fsanitize=address -fsanitize=undefined" LDFLAGS="-fsanitize=address -fsanitize=undefined")
+fi
+./configure "${CONFIGURE_ARGS[@]}"
+make
+make distcheck
