388843e82deb56ef37954bfafc3c5924b5ff7e8c
Reuben Thomas <rrt@sc3d.org>
2021-11-23 20:52:46 +0000

    AppVeyor: Get verbose build commands in logs

--- build-aux/appveyor-build.sh
+++ build-aux/appveyor-build.sh
@@ -11,5 +11,5 @@ if [[ "$ASAN" == "yes" ]]; then
     CONFIGURE_ARGS+=(CFLAGS="-g3 -fsanitize=address -fsanitize=undefined" CXXFLAGS="-g3 -fsanitize=address -fsanitize=undefined" LDFLAGS="-fsanitize=address -fsanitize=undefined")
 fi
 ./configure "${CONFIGURE_ARGS[@]}"
-make
+make V=1
 make distcheck
