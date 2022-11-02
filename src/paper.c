a0f030806e3fed69161ec43a128e3ca437d89a4b
Reuben Thomas <rrt@sc3d.org>
2022-11-02 18:38:52 +0000

    Fix remaining licensing nits in paper.c and tbl_opts.h
    Thanks, @bgermann!

--- src/paper.c
+++ src/paper.c
@@ -7,7 +7,7 @@
 
   This program is free software: you can redistribute it and/or modify it
   under the terms of the GNU General Public License as published by
-  the Free Software Foundation; either version 2 of the License, or (at
+  the Free Software Foundation; either version 3 of the License, or (at
   your option) any later version.
 
   This program is distributed in the hope that it will be useful,
@@ -17,7 +17,7 @@
 
   You should have received a copy of the GNU General Public License
   along with this program.  If not, see
-  <https://www.gnu.org/licenses/lgpl-2.1.html>.
+  <https://www.gnu.org/licenses/gpl-3.0.html>.
 */
 
 #include "config.h"
